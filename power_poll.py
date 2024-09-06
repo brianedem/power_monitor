52db1eb53beebe0350a8c2653aa890c5a9199bc1
Brian Edem <bcedem@gmail.com>
2024-09-06 08:07:40 -0700

    Refined error handling; now filters output to file

--- power_poll.py
+++ power_poll.py
@@ -1,38 +1,76 @@
 #!/usr/bin/python3
 import logging
-logger = logging.getLogger(__name__)
-logging.basicConfig(filename='power_poll.log', encoding='utf-8', level=logging.DEBUG)
-
 import socket
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
+import json
+import time
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='power_poll.log', encoding='utf-8', level=logging.INFO)
+
+    # if 'DNS Assist' is enabled in the network privicy settings then
+    # host names with the .local suffix are forwarded to their DNS server
+    # The workaround is to use .attlocal.net instead, but which to use?
+    # The 'search' item in /etc/resolve.conf will be set to attlocal.net
+    # if DNS Assist is enabled
+try :
+    with open('/etc/resolve.conf') as fd:
+        for line in fd :
+            if line.startswith('search') :
+                search = '.'+line.split()[1]
+                break
+except FileNotFoundError :
+    search = ''     # maybe this should be '.local'?
 
 timeout = 2
 socket.setdefaulttimeout(timeout)
 
-reqC = Request('http://condenser/data.json')
-reqE = Request('http://evaporator/data.json')
+devices = ('condenser', 'evaporator', 'waterheater')
+#devices = ('condenser', 'evaporator')
 
-import json
+mapping = {}
 
-def pollDevice(req) :
-    values = None
+def pollDevice(dev) :
+    req = Request(f'http://{dev+search}/data.json')
+    values = {}
     try:
         response = urlopen(req)
     except HTTPError as e:
-        logger.exception(e.code)
+        logger.exception(f'Request to {dev} {e.code}')
     except URLError as e:
-        logger.exception(e.reason)
+        logger.exception(f'Request to {dev} {e.reason}')
+    except TimeoutError :
+        logger.exception(f'Request to {dev} timed out')
     else :
         content_type = response.getheader('Content-type')
         if 'json' in content_type :
             body = response.read()
-            logger.info(body)
+            logger.debug(body)
             values = json.loads(body)
+        else :
+            print('{dev}: json content not found')
+            print(response.read())
     return values
 
-for i in range(2) :
-    print('---')
-    for device in (reqC, reqE) :
-        values = pollDevice(device)
-        print(values)
+def extract(data, values) :
+    result = {}
+    for value in values :
+        if value in data :
+            result[value] = data[value]
+    return result
+
+fd = open('data.json', 'a', encoding='utf-8')
+
+while True :
+    responses = {'time': time.localtime()}
+    for device in devices :
+        values = extract(pollDevice(device), ('voltage', 'current', 'power', 'energy', 'temperature'))
+        if 'temperature' in values and values['temperature'] > 55 :
+            del values['temperature']
+        responses[device] = values
+
+#   print(json.dumps(responses))
+    fd.write(json.dumps(responses)+'\n')
+    fd.flush()
+    time.sleep(5)

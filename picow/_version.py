771d4ead5dd8b38b72694f0ad530f83df989dcc2
Brian Edem <bcedem@gmail.com>
2024-09-25 08:58:10 -0700

    Bumped minor version

--- picow/_version.py
+++ picow/_version.py
@@ -1,3 +1,3 @@
-version = "3.0.0"
-releaseDate = "Mon Aug 26 15:33:06 2024"
-gitRevision = "1186773e9180959cc8e6a2e58529949c004411d6"
+version = "3.1.0"
+releaseDate = "Wed Sep 25 08:57:33 2024"
+gitRevision = "a724658e3f7f072aaa826d54a82132c130290cf2"

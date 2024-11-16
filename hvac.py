fd7ba097e3fad897ab02e31ed40027ba903a8083
Brian Edem <bcedem@gmail.com>
2024-11-15 16:25:29 -0800

    Added logging of daily power to database

--- hvac.py
+++ hvac.py
@@ -9,36 +9,63 @@ daily power consumption are logged
 """
 
 import logging
-import requests
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 import json
 import time
 from enum import Enum
-import random
-import string
+import platform
+import argparse
+import sys
 
-SAMPLE_INTERVAL = 5     # seconds
+# handle command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--detail", help="print details of HVAC activity",
+    action="store_true")
+parser.add_argument("--daily", help="log daily total power to database",
+    action="store_true")
+parser.add_argument("--sql_test", help="test SQL power logging",
+    action="store_true")
+args = parser.parse_args()
+
+# determine if running on macBook
+MACOS = platform.system()=='Darwin'
+
+#default behavior controls
+MYSQL = False
+SAMPLE_INTERVAL = 60    # interval to read the power meter
+PERIODIC_INTERVAL = 30  # interval to poll when HVAC system is on and power is stable
+
+if args.daily and not MACOS:
+    # missing Mariadb connector for MACOS
+    MYSQL = True
+if args.detail:
+    # increase sampling rate
+    SAMPLE_INTERVAL = 5
+
+if args.sql_test:
+    if MACOS:
+        print('unable to test sql on MACOS platform')
+        sys.exit()
+    MYSQL = True
+    test_time = 8*60*60     # test timer that starts in 1970 and increments daily
+    SAMPLE_INTERVAL = 5
 
 log = logging.getLogger(__name__)
-logging.basicConfig(filename='hvac.log', encoding='utf-8', level=logging.WARN)
+if args.sql_test:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(filename='hvac.log', encoding='utf-8', level=logging.WARN)
 
-# the ntfy key is stored in a separate file for security
-config_file = 'wheater.cfg'
-try:
-    fd = open(config_file)
-    config_data = json.load(fd)
-    ntfy_key = config_data['ntfy_key']
-except FileNotFoundError:
-    # generate a mostly random lower-case string (easy to enter on iphone)
-    ntfy_key = 'wh' + ''.join(random.choices(string.ascii_lowercase, k=14))
-    config_data = {'ntfy_key': ntfy_key}
-    with open(config_file, mode='w', encoding='utf-8') as fd:
-        print('creating config file')
-        json.dump(config_data, fd)
-
-print(f'using {ntfy_key} as nfty key')
-
+sql_database = 'elpowerdb'
+if MYSQL:
+    import mariadb
+    try:
+        mdb = mariadb.connect(user='power_update', database=sql_database)
+        mc = mdb.cursor()
+    except mariadb.OperationalError:
+        MYSQL = False
+        log.exception(f'Unable to connect to {sql_database} database')
 
 def pollDevice(dev) :
     """ Reads power information from picow + peacefair power meter
@@ -98,7 +125,7 @@ while True:
     time.sleep(SAMPLE_INTERVAL)
 
     # capture time that measurement was made
-    current_time_sec = time.time()                   # in seconds since epoc
+    current_time_local = time.localtime()   # time structure
 
     # read the power meters
     errors = []
@@ -153,23 +180,47 @@ while True:
         # without intervention, but should include .
         continue
 
+
     # report summary of yesterday's energy use
     # TODO add to database?
-    current_time_local = time.localtime(current_time_sec)   # extract day of year
-    if current_time_local.tm_yday != day_start_time.tm_yday:
-        timestamp = time.strftime('%a %b %d', day_start_time)
-        c_energy = condenser.energy - condenser.day_start_energy
-        e_energy = evaporator.energy - evaporator.day_start_energy
-        energy_used = c_energy + e_energy
-        print(f'{timestamp}      {c_energy:.1f} + {e_energy:.1f} = {energy_used:.1f} kWh used yesterday')
-        day_start_time = current_time_local
-        condenser.day_start_energy = condenser.energy
-        evaporator.day_start_energy = evaporator.energy
+    if MYSQL:
+        if args.sql_test:
+            new_day = True
+            timestamp = time.strftime('%a %b %d', time.localtime(test_time))
+            sql_timestamp = time.strftime('%Y-%m-%d', time.localtime(test_time))
+            test_time += 24*60*60   # advance a day
+        else:
+            new_day = current_time_local.tm_yday != day_start_time.tm_yday 
+            timestamp = time.strftime('%a %b %d', day_start_time)
+            sql_timestamp = time.strftime('%Y-%m-%d', day_start_time)
+            day_start_time = current_time_local
+        if new_day:
+            c_energy = condenser.energy - condenser.day_start_energy
+            e_energy = evaporator.energy - evaporator.day_start_energy
+            energy_used = c_energy + e_energy
+            print(f'{timestamp}      {c_energy:.1f} + {e_energy:.1f} = {energy_used:.1f} kWh used yesterday')
+            if MYSQL:
+                try:
+                    sql_command = 'INSERT INTO hvac_power (day, condensor_energy, evaporator_energy)' \
+                        f' VALUES ("{sql_timestamp}", {c_energy}, {e_energy})'
+                    print(sql_command)
+                    mc.execute(sql_command)
+                    mdb.commit()
+                except mariadb.Error:
+                    log.error("unable to update hvac_power table in elpowerdb")
+                    sys.exit()
+
+            condenser.day_start_energy = condenser.energy
+            evaporator.day_start_energy = evaporator.energy
+
+    # skip the following if not reporting power details
+    if not args.detail:
+        continue
 
     # start timers every time a change in load is detected
     if power_change_detected:
         # generate messages for this many seconds after the last detected change
-        power_change_timer = 100
+        power_change_timer = PERIODIC_INTERVAL
         # periodically generate messages while the system is running
         run_time = 0
 

2b57759e017556c3d2625030d0fc7e8ee91c69de
Brian Edem <bcedem@gmail.com>
2024-12-03 08:16:10 -0800

    Edit from before the polling function was split into energy_monitor. Not tested

--- power_poll.py
+++ power_poll.py
@@ -1,58 +1,74 @@
-#!/usr/bin/python3
+#!./venv/bin/python
+""" Home Power Logger
+
+This script will read the cumulative energy readings of the specified
+power meters and log the values in a database.
+
+This script should be run daily via cron
+"""
+
+import os
 import logging
 import socket
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 import json
 import time
+import platform
+import sys
+import mariadb
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(filename='power_poll.log', encoding='utf-8', level=logging.DEBUG)
+program_name = os.path.basename(sys.argv[0])
+log_file = os.path.splitext(program_name)[0] + '.log'
 
-    # if 'DNS Assist' is enabled in the network privicy settings then
-    # host names with the .local suffix are forwarded to their DNS server
-    # The workaround is to use .attlocal.net instead, but which to use?
-    # The 'search' item in /etc/resolve.conf will be set to attlocal.net
-    # if DNS Assist is enabled
-try :
-    with open('/etc/resolve.conf') as fd:
-        for line in fd :
-            if line.startswith('search') :
-                search = '.'+line.split()[1]
-                break
-except FileNotFoundError :
-    search = ''     # maybe this should be '.local'?
+log = logging.getLogger(__name__)
+logging.basicConfig(filename=log_file, encoding='utf-8', level=logging.DEBUG)
+
+# determine if running on macBook
+MACOS = platform.system()=='Darwin'
+
+if MACOS:
+    log.exception('power_poll does not run on a MacBook')
+    sys.exit()
+
+# database
+sql_database = 'elpowerdb'
+try:
+    mdb = mariadb.connect(user='power_update', database=sql_database)
+    mc = mdb.cursor()
+except mariadb.OperationalError:
+    log.exception(f'Unable to connect to {sql_database} database')
+    sys.exit()
 
 timeout = 2
 socket.setdefaulttimeout(timeout)
 
-devices = ('condenser', 'evaporator', 'waterheater')
+devices = (
+    'condenser',
+    'evaporator',
+    'waterheater',
+    )
 
-m = {}
-for device in devices:
-    ip = socket.gethostbyname(device)
-    m[device] = ip
-print(m)
 def pollDevice(dev) :
-    req = Request(f'http://{dev+search}/data.json')
+    req = Request(f'http://{dev}.lan/data.json')
     values = {}
     try:
         with urlopen(req) as response :
             content_type = response.getheader('Content-type')
             if 'json' in content_type :
                 body = response.read()
-                logger.debug(body)
+                log.debug(body)
                 values = json.loads(body)
             else :
                 print('{dev}: json content not found')
                 print(response.read())
 
     except HTTPError as e:
-        logger.exception(f'Request to {dev} {e.code}')
+        log.exception(f'Request to {dev} {e.code}')
     except URLError as e:
-        logger.exception(f'Request to {dev} {e.reason}')
+        log.exception(f'Request to {dev} {e.reason}')
     except TimeoutError :
-        logger.exception(f'Request to {dev} timed out')
+        log.exception(f'Request to {dev} timed out')
 
     return values
 
@@ -63,19 +79,18 @@ def extract(data, values) :
             result[value] = data[value]
     return result
 
-fd = open('data.json', 'a', encoding='utf-8')
+resp = {}
+for device in devices :
+    values = pollDevice(device)
+    try:
+        energy = values['energy']
+    except:
+        energy = None
+        log.warning(f'Missing value for {device}')
+    resp[device] = energy
 
-while True :
-    responses = {'time': time.localtime()}
-    for device in devices :
-        values = extract(pollDevice(m[device]), ('voltage', 'current', 'power', 'energy', 'temperature'))
-        if 'temperature' in values :
-            value = values['temperature']
-            if isinstance(value, str) :
-                del values['temperature']
-        responses[device] = values
-
-#   print(json.dumps(responses))
-    fd.write(json.dumps(responses)+'\n')
-    fd.flush()
-    time.sleep(5)
+print(resp)
+sql_timestamp = time.strftime('%Y-%m-%d')
+sql_command = 'INSERT INTO hvac_power (day, condensor_energy, evaporator_energy, wh_energy)' \
+                        f' VALUES ("{sql_timestamp}",' \
+                        f' {resp["condensor"]}, {resp["evaporator"]})' 
